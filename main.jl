e8e8a4eed2521f77c58851ee84c603a421d733f8
Nikhil Nayak <nikhil.a.nayak@gmail.com>
2022-07-15 09:40:46 -0700

    Added FFTs

--- main.jl
+++ main.jl
@@ -1,10 +1,11 @@
-using Plots
+using FFTW
+
+using PyPlot
 
 A =   [200, 90, 30, 17, 8, 3.5, 1]
 Phi = [10, 20, 30, 40, 50, 60, 70]
 
 f = [0.00009259, 0.00020833, 0.00047619, 0.00111111, 0.00238095, 0.00555555, 0.025]
-
 
 function SMFV(x)
 	y = 0
@@ -19,27 +20,13 @@ function SMFV(x)
 end
 
 
-function dSMFV(x)
-	y = 0
-	for i in 1:7
-		y += 2 * pi * f[i] * A[i] * cos(
-				2 * pi * f[i] * x 
-				+ (pi / 180 * Phi[i])
-			    )
-	end
+timestep = 1000
 
-	return y
-end
-
-
-
-X = 0:0.1:86400
+X = 0:timestep:86400
 Y = SMFV.(X)
 
+f = fft(Y)
+freq = fftfreq(length(Y), timestep)
 
-# println(Y)
-
-savefig(plot(X, Y), "out.png")
-
-
+plot(freq, abs.(f))
 
