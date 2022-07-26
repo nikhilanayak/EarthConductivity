23e1f1a3dfbb7884e45fa08599501b2c22472f40
Nikhil Nayak <nikhil.a.nayak@gmail.com>
2022-07-26 14:51:46 -0700

    Comments

--- eq_19.jl
+++ eq_19.jl
@@ -3,31 +3,31 @@ res = [20000, 200, 1000, 100]
 #Sigmas = [1, 1, 1, 1, 1]
 Sigmas = [1/20000, 1/200, 1/1000, 1/100, 1/3]
 
-N = 5 # num layers
+N = 5 # number of layers in the model
 
-mu = 4 * pi * 10^-7
-i = 1im
+mu = 4 * pi * 10^-7 # µ, defined below equation 2
+i = 1im # √-1
 
 
-function K_N(f)
+function K_N(f) # top layer of the model, equation 16
 	return sqrt((i * 2 * pi * f) / (mu * Sigmas[N]))
 end
 
 
-function k(n, f)
+function k(n, f) # equation 4
 	return sqrt(i * 2 * pi * f * mu * Sigmas[n])
 end
 
-function eta(n, f)
+function eta(n, f) # η, defined below equation 18
 	return i * 2 * pi * f / k(n, f)
 end
 
 
-function eExpression(n, f)
+function eExpression(n, f) # e ^ (-2knln)
 	return ℯ ^ (-2 * k(n, f) * Ls[n])
 end
 
-function K(n, f)
+function K(n, f) # Equation 19
 	#println("in layer ", n)
 	if n == N
 		return K_N(f)
@@ -43,6 +43,7 @@ function K(n, f)
 end
 
 
-println(
-	K(1, 1)
-)
+Kf = K(1, 0.025)
+
+println(abs(Kf))
+println(angle(Kf)*180/pi)
